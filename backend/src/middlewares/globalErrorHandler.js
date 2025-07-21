028650722ce73b4216701c00a6bc8d2de90b5854
Ashutosh kumar <115258392+Ashukr321@users.noreply.github.com>
2025-07-21 18:40:28 +0530

    handle global error handler middleware

---
+++ backend/src/middlewares/globalErrorHandler.js
@@ -0,0 +1,18 @@
+import createError from 'http-errors'
+
+// create globalErrorHandler Function 
+const globalErrorHandler = (err, req, res, next) => {
+  if (!err.status) {
+    const err = createError(500, "Internal Server Error");
+    return next(err);
+  }
+
+  // send response 
+  return res.status(err.status).json({
+    success: false,
+    message: err.message || "Something Went Wrong!"
+  })
+}
+
+
+export default globalErrorHandler;
